b87531802ce4a4c14fd5f7462a39777480a8ff5f
jsw7524 <jsw7524@gmail.com>
2018-06-22 18:10:38 +0800

    AC

--- 443. String Compression/Program.cs
+++ 443. String Compression/Program.cs
@@ -6,6 +6,42 @@ using System.Threading.Tasks;
 
 namespace _443.String_Compression
 {
+    public class Solution
+    {
+        public int Compress(char[] chars)
+        {
+            char previousChar = chars.First();
+            int count = 1;
+            int  charPtr = 0;
+            foreach (var c in chars.Skip(1))
+            {
+                if (previousChar == c)
+                {
+                    count++;
+                }
+                else
+                {
+                    chars[charPtr++] = previousChar;
+                    if (count > 1)
+                    {
+                        Array.Copy(count.ToString().ToCharArray(),0, chars, charPtr, count.ToString().Length);
+                        charPtr += count.ToString().Length;
+                    }
+                    previousChar = c;
+                    count = 1;
+
+                }
+            }
+            chars[charPtr++] = previousChar;
+            if (count > 1)
+            {
+                Array.Copy(count.ToString().ToCharArray(), 0, chars, charPtr, count.ToString().Length);
+                charPtr += count.ToString().Length;
+            }
+            return charPtr;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using _443.String_Compression;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Solution sln = new Solution();
+            char[] testData = new char[] { 'a', 'a', 'b', 'b', 'c', 'c', 'c' };
+            sln.Compress(testData);
+            Assert.AreEqual(JsonConvert.SerializeObject(new char[] { 'a', '2', 'b', '2', 'c', '3','c' }),JsonConvert.SerializeObject(testData));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Solution sln = new Solution();
+            char[] testData = new char[] { 'a',  'b', 'c' };
+            sln.Compress(testData);
+            Assert.AreEqual(JsonConvert.SerializeObject(new char[] { 'a', 'b',  'c' }), JsonConvert.SerializeObject(testData));
+        }
+
+    }
+}
